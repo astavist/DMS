659c919893ba2c1d3a2622f1ca7f5a52c57c1625
Samet Yılmaz <yilmazsamet98@gmail.com>
2024-06-08 18:43:28 +0300

    Update: Adapt for new dtos

--- src/main/java/com/astavist/DMS/controller/DiaryEntryController.java
+++ src/main/java/com/astavist/DMS/controller/DiaryEntryController.java
@@ -1,49 +1,43 @@
 package com.astavist.DMS.controller;
 
-import com.astavist.DMS.model.DiaryEntry;
-import com.astavist.DMS.service.DiaryEntryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.astavist.DMS.service.DiaryEntryManager;
+import com.astavist.DMS.service.dto.requests.CreateDiaryRequest;
+import com.astavist.DMS.service.dto.requests.UpdateDiaryRequest;
+import com.astavist.DMS.service.dto.responses.CreateDiaryResponse;
+import com.astavist.DMS.service.dto.responses.GetAllDiariesResponse;
+import com.astavist.DMS.service.dto.responses.GetDiaryResponse;
+import com.astavist.DMS.service.dto.responses.UpdateDiaryResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/diary-entries")
 @CrossOrigin(origins = "http://localhost:3000")
+@AllArgsConstructor
 public class DiaryEntryController {
-    private final DiaryEntryService diaryEntryService;
-
-    @Autowired
-    public DiaryEntryController(DiaryEntryService diaryEntryService) {
-        this.diaryEntryService = diaryEntryService;
-    }
+    private final DiaryEntryManager service;
 
     @PostMapping
-    public ResponseEntity<DiaryEntry> createEntry(@RequestBody DiaryEntry diaryEntry) {
-        diaryEntry.setCreatedDate(LocalDateTime.now());
-        DiaryEntry savedEntry = diaryEntryService.saveEntry(diaryEntry);
-        return ResponseEntity.ok(savedEntry);
+    public CreateDiaryResponse createEntry(@RequestBody CreateDiaryRequest request) {
+        return service.create(request);
     }
-
     @GetMapping
-    public ResponseEntity<List<DiaryEntry>> getAllEntries() {
-        List<DiaryEntry> diaryEntries = diaryEntryService.getAllEntries();
-        return ResponseEntity.ok(diaryEntries);
+    public List<GetAllDiariesResponse> getAllEntries() {
+        return service.getAll();
     }
-
     @GetMapping("/{id}")
-    public ResponseEntity<DiaryEntry> getEntryById(@PathVariable UUID id) {
-        Optional<DiaryEntry> diaryEntry = diaryEntryService.getEntryById(id);
-        return diaryEntry.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public GetDiaryResponse getEntryById(@PathVariable UUID id) {
+        return service.getById(id);
     }
-
+    @PutMapping("/{id}")
+    public UpdateDiaryResponse updateEntry(UUID id, UpdateDiaryRequest request){
+        return service.update(id,request);
+    }
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEntry(@PathVariable UUID id) {
-        diaryEntryService.deleteEntry(id);
-        return ResponseEntity.noContent().build();
+    public void deleteEntry(@PathVariable UUID id) {
+        service.delete(id);
     }
 }
