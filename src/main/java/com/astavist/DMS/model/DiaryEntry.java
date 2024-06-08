1b1f8d41684c1206d0b04461cb0767cc16d7f7a5
Samet Yılmaz <yilmazsamet98@gmail.com>
2024-06-08 17:18:26 +0300

    Add: Lombok to Model

--- src/main/java/com/astavist/DMS/model/DiaryEntry.java
+++ src/main/java/com/astavist/DMS/model/DiaryEntry.java
@@ -1,11 +1,19 @@
 package com.astavist.DMS.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DiaryEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -18,44 +26,4 @@ public class DiaryEntry {
 
     private LocalDateTime createdDate;
 
-    public DiaryEntry() {
-    }
-
-    public DiaryEntry(String title, String content, LocalDateTime createdDate) {
-        this.title = title;
-        this.content = content;
-        this.createdDate = createdDate;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
 }
