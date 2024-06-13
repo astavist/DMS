de2622930bd679e4423beeb52e118a57cf2a8e76
Samet Yılmaz <yilmazsamet98@gmail.com>
2024-06-13 20:23:20 +0300

    Edit: File Format

--- src/main/java/com/astavist/DMS/configuration/ModelMapper/ModelMapperConfig.java
+++ src/main/java/com/astavist/DMS/configuration/ModelMapper/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package com.astavist.DMS.configuration;
+package com.astavist.DMS.configuration.ModelMapper;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
